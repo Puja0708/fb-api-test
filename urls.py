c4116be706647ea5c384be0564b3d61ed5f11cda
puja_sk <puja@sportskeeda.com>
2016-01-07 18:47:34 +0530

    json data rendered on template

--- urls.py
+++ urls.py
@@ -4,23 +4,28 @@
 # from flask import render_template
 from flask import Flask, Markup
 from flask import render_template
-from views import get_number_of_likes_on_post, get_comment_count_on_post, get_author_name, get_data_from_page_id, get_post_title
+# from views import get_number_of_likes_on_post, get_author_name, get_data_from_page_id
+from views import get_json_test
 
 app = Flask(__name__)
-
-# @app.route('/')
+data = ''
+@app.route('/')
 def hello_world():
-	for i in xrange(0,25):
+	data = get_json_test()
+	# print data
+	return render_template('index.html', page_name='SportsKeeda', data=data)
+	# for i in xrange(0,25):
 		# id = get_data_from_page_id()
 		# id = str(id)
 		# post_id = '116151972998_10153809342577999'
-		for i in xrange(0,25):
-			post_id = get_data_from_page_id(i)
-			likes = get_number_of_likes_on_post(post_id) + i
-			post_title = get_post_title(post_id)
-			author = get_author_name(post_id)
-			print post_id, likes, post_title, author
-			return render_template('index.html', page_name='SportsKeeda',likes=likes, post_id=post_id, post_title=post_title, author=author)
+		# for i in xrange(0,25):
+			# post_id = get_data_from_page_id(i)
+			# likes = get_number_of_likes_on_post(post_id) + i
+			# post_title = get_post_title(post_id)
+			# author = get_author_name(post_id)
+			# post_title, author = get_author_name(post_id)
+			# print post_id, likes, post_title, author
+			# return render_template('index.html', page_name='SportsKeeda',likes=likes, post_id=post_id, post_title=post_title, author=author)
 		# return render_template('index.html', page_name='SportsKeeda',likes=likes, post_id=post_id, post_title=post_title, author=author)
 	# return 'Hello World!'
 
@@ -28,6 +33,12 @@ def hello_world():
 # def test_json_data():
 # 	return render_template('test_json.html')
 
-# if __name__ == '__main__':
-# 	app.run(port=1234, debug=True)
-hello_world()
+@app.route('/foo')
+def next():
+	data1 = get_json_test()
+	# print data
+	return render_template('index.html', data1=data1)
+
+if __name__ == '__main__':
+	app.run(port=1234, debug=True)
+# hello_world()
--- views.py
+++ views.py
@@ -5,11 +5,13 @@ import json
 import requests
 
 
-ACCESS_TOKEN = 'CAACEdEose0cBALItxv1Kau2TSzGJEsIJCdl3eQZBlZCkLc2ZCQaVtIZBNLMNlEZCiOt997FKvTZAmH7KZBmnMsiMfSMy6tSV132xpxYN466QQhRFjhJIg1IxtWjkLfNdIoHQv9VIUNGPLwINm2EUSq0cRZCJX7aO01oxocga4o4aGWCn8BB5o9lm9qbcKENlrzLtBJMdIg8TMQZDZD'
+ACCESS_TOKEN = 'CAACEdEose0cBANXHOluTccNW8WoPZCRWr6EtRw7LEo4GCX4D7NYwZB3U25NBGFsxVU95IJly3IXDNZBE6ZAmSUwEfclHqqIBLhGMnytbrDxSqSb4oQOoWi8QsduyC4IPjwz1ZAMTF0l2wb9oAE9xmHWu6scGK3pro7rGAZBRWLcyM5JTW8WyHMSaIwh4qksavyIcL4nKdEZBgZDZD'
 DEBUG = True
 app = Flask(__name__)
 app.debug = DEBUG
 oauth = OAuth()
+data = {}
+data1 = []
 # json_array = []
 
 base_url = 'https://graph.facebook.com/v2.2/'
@@ -18,14 +20,19 @@ post = '116151972998_10153804678607999?access_token='
 
 #we are using the facebook graph API version 2.2, instead of the latest one(2.5)
 def get_data_from_page_id(i):
-	url = 'https://graph.facebook.com/v2.2/116151972998/posts?access_token=CAACEdEose0cBALItxv1Kau2TSzGJEsIJCdl3eQZBlZCkLc2ZCQaVtIZBNLMNlEZCiOt997FKvTZAmH7KZBmnMsiMfSMy6tSV132xpxYN466QQhRFjhJIg1IxtWjkLfNdIoHQv9VIUNGPLwINm2EUSq0cRZCJX7aO01oxocga4o4aGWCn8BB5o9lm9qbcKENlrzLtBJMdIg8TMQZDZD'
+	
+	url = 'https://graph.facebook.com/v2.2/116151972998/posts?access_token=CAACEdEose0cBANXHOluTccNW8WoPZCRWr6EtRw7LEo4GCX4D7NYwZB3U25NBGFsxVU95IJly3IXDNZBE6ZAmSUwEfclHqqIBLhGMnytbrDxSqSb4oQOoWi8QsduyC4IPjwz1ZAMTF0l2wb9oAE9xmHWu6scGK3pro7rGAZBRWLcyM5JTW8WyHMSaIwh4qksavyIcL4nKdEZBgZDZD'
 	response = requests.get(url)
 	json_data = json.loads(response.text)
 	data1 = json_data["data"]
-	# post_id_to_be_returned = data1[i]["id"]
-	# return post_id_to_be_returned
-	post_id_to_be_returned = data1[i]["id"]
-	title = data1[i]["name"]
+		# post_id_to_be_returned = data1[i]["id"]
+		# return post_id_to_be_returned
+		# post_id_to_be_returned = data1[i]["id"]
+	# title = data1[i]["name"]
+	if i>25:
+		if 'next' in json_data['paging']:
+			url = json_data["paging"]["next"]
+			post_id_to_be_returned = url
 	# print i, title, post_id_to_be_returned
 	return post_id_to_be_returned
 	
@@ -40,12 +47,12 @@ def get_data_from_page_id(i):
 	#generate access token via oauth, once done
 	
 
-def get_data_from_page_id_posts():
-	return "hello"
+# def get_data_from_page_id_posts():
+# 	return "hello"
 
 def get_data_from_post_id():
 	# return "hello"
-	url = 'https://graph.facebook.com/v2.2/116151972998/posts?access_token=CAACEdEose0cBALItxv1Kau2TSzGJEsIJCdl3eQZBlZCkLc2ZCQaVtIZBNLMNlEZCiOt997FKvTZAmH7KZBmnMsiMfSMy6tSV132xpxYN466QQhRFjhJIg1IxtWjkLfNdIoHQv9VIUNGPLwINm2EUSq0cRZCJX7aO01oxocga4o4aGWCn8BB5o9lm9qbcKENlrzLtBJMdIg8TMQZDZD'
+	url = 'https://graph.facebook.com/v2.2/116151972998/posts?access_token=CAACEdEose0cBANXHOluTccNW8WoPZCRWr6EtRw7LEo4GCX4D7NYwZB3U25NBGFsxVU95IJly3IXDNZBE6ZAmSUwEfclHqqIBLhGMnytbrDxSqSb4oQOoWi8QsduyC4IPjwz1ZAMTF0l2wb9oAE9xmHWu6scGK3pro7rGAZBRWLcyM5JTW8WyHMSaIwh4qksavyIcL4nKdEZBgZDZD'
 	response = requests.get(url)
 	json_data = json.loads(response.text)
 	number_of_posts = len(json_data["data"])
@@ -64,28 +71,40 @@ def get_number_of_likes_on_post(id):
 	return number_of_likes
 	# return number_of_likes
 
-def get_comment_count_on_post():
-	url = 'https://graph.facebook.com/v2.2/116151972998_10153804678607999/comments?access_token=CAACEdEose0cBALItxv1Kau2TSzGJEsIJCdl3eQZBlZCkLc2ZCQaVtIZBNLMNlEZCiOt997FKvTZAmH7KZBmnMsiMfSMy6tSV132xpxYN466QQhRFjhJIg1IxtWjkLfNdIoHQv9VIUNGPLwINm2EUSq0cRZCJX7aO01oxocga4o4aGWCn8BB5o9lm9qbcKENlrzLtBJMdIg8TMQZDZD'
-	response = requests.get(url)
-	json_data = json.loads(response.text)
-	number_of_comments = len(json_data["data"])
+# def get_comment_count_on_post():
+# 	url = 'https://graph.facebook.com/v2.2/116151972998_10153804678607999/comments?access_token=CAACEdEose0cBAE9nSzuhGXXQt8ZCycutnBq9JwlZA5tXImoShRbyDFa3LcxmVwpRVddbOEhYvHnVcf3Q6kndOe9pZBNplloeG1plM1viANggohdgmf8ALA0fefkiyZC8cm2TXSOmnmJe1Nqe8ElNWhY7yratd3SFraKKwQ6tQQ1mc0lP3tVAVsRkiTJYDeZAjs00DPOAkKwZDZD'
+# 	response = requests.get(url)
+# 	json_data = json.loads(response.text)
+# 	number_of_comments = len(json_data["data"])
 	
 
 def get_author_name(id):
 	# url = base_url + post + ACCESS_TOKEN
-	url = 'https://graph.facebook.com/v2.2/' + id +'?access_token=CAACEdEose0cBALItxv1Kau2TSzGJEsIJCdl3eQZBlZCkLc2ZCQaVtIZBNLMNlEZCiOt997FKvTZAmH7KZBmnMsiMfSMy6tSV132xpxYN466QQhRFjhJIg1IxtWjkLfNdIoHQv9VIUNGPLwINm2EUSq0cRZCJX7aO01oxocga4o4aGWCn8BB5o9lm9qbcKENlrzLtBJMdIg8TMQZDZD'
+	url = 'https://graph.facebook.com/v2.2/' + id +'?access_token=CAACEdEose0cBANXHOluTccNW8WoPZCRWr6EtRw7LEo4GCX4D7NYwZB3U25NBGFsxVU95IJly3IXDNZBE6ZAmSUwEfclHqqIBLhGMnytbrDxSqSb4oQOoWi8QsduyC4IPjwz1ZAMTF0l2wb9oAE9xmHWu6scGK3pro7rGAZBRWLcyM5JTW8WyHMSaIwh4qksavyIcL4nKdEZBgZDZD'
 	response = requests.get(url)
 	json_data = json.loads(response.text)
 	author_name = json_data["admin_creator"]["name"]
-	return author_name
+	# post_id = json_data["id"]
+	title = json_data["name"]
+	return title, author_name
 
-def get_post_title(id):
-	url = 'https://graph.facebook.com/v2.2/' + id +'?access_token=CAACEdEose0cBALItxv1Kau2TSzGJEsIJCdl3eQZBlZCkLc2ZCQaVtIZBNLMNlEZCiOt997FKvTZAmH7KZBmnMsiMfSMy6tSV132xpxYN466QQhRFjhJIg1IxtWjkLfNdIoHQv9VIUNGPLwINm2EUSq0cRZCJX7aO01oxocga4o4aGWCn8BB5o9lm9qbcKENlrzLtBJMdIg8TMQZDZD'
-	# print url
+def get_data_for_json(id):
+	# url = base_url + post + ACCESS_TOKEN
+	url = 'https://graph.facebook.com/v2.2/' + id +'?access_token=CAACEdEose0cBANXHOluTccNW8WoPZCRWr6EtRw7LEo4GCX4D7NYwZB3U25NBGFsxVU95IJly3IXDNZBE6ZAmSUwEfclHqqIBLhGMnytbrDxSqSb4oQOoWi8QsduyC4IPjwz1ZAMTF0l2wb9oAE9xmHWu6scGK3pro7rGAZBRWLcyM5JTW8WyHMSaIwh4qksavyIcL4nKdEZBgZDZD'
 	response = requests.get(url)
 	json_data = json.loads(response.text)
+	author_name = json_data["admin_creator"]["name"]
+	# post_id = id
 	title = json_data["name"]
-	return title	
+	return title, author_name
+
+# def get_post_title(id):
+# 	url = 'https://graph.facebook.com/v2.2/' + id +'?access_token=CAACEdEose0cBAE9nSzuhGXXQt8ZCycutnBq9JwlZA5tXImoShRbyDFa3LcxmVwpRVddbOEhYvHnVcf3Q6kndOe9pZBNplloeG1plM1viANggohdgmf8ALA0fefkiyZC8cm2TXSOmnmJe1Nqe8ElNWhY7yratd3SFraKKwQ6tQQ1mc0lP3tVAVsRkiTJYDeZAjs00DPOAkKwZDZD'
+# 	# print url
+# 	response = requests.get(url)
+# 	json_data = json.loads(response.text)
+# 	title = json_data["name"]
+# 	return title	
 	# print url
 
 
@@ -105,14 +124,79 @@ def get_post_title(id):
 # get_data_from_page_id()
 # get_number_of_likes_on_post('116151972998_10153809342577999')
 # get_post_title('116151972998_10153809426852999')
-def get_json_all():
-	data = {}
-	data["post_id"]='aaa'
-	data['author_name'] = 'bbb'
-	print data
-	json_data1 = json.loads(json.dumps(data, ensure_ascii=False))
-	print json_data1
-	print json_data1["post_id"]
+def get_json_all(i):
+	post_id = get_data_from_page_id(i)
+		# print i, post_id
+		# 
+		# post_title, author = get_author_name(post_id)
+	post_id = post_id
+	post_title, author = get_author_name(post_id)
+		# print post_id, post_title, author
+	likes = get_number_of_likes_on_post(post_id) + i
+
+		# data["post_id"] = post_id
+		# print post_id
+	data["post_id"] = post_id
+	data["title"] = post_title		
+	data["author_name"] = author
+	data["likes"] = likes
+	return data
+
+# def json_data_test():
+# 	for i in range(0,5):
+# 		get_json_all(i)
+
+		# print data1
+		# print data["post_id"], data["title"], data["author_name"], data["likes"]
+	# print data
+	# print data1
+	# json_data = json.loads(json.dumps(data1, ensure_ascii=False))
+	# return json_data
+
+	# data["post_id"]='aaa'
+	# data['author_name'] = 'bbb'
+	# print data
+	# json_data1 = json.loads(json.dumps(data, ensure_ascii=False))
+	# print json_data1
+	# print json_data1["post_id"]
+	# for i in range(0,25):
+	# 	data["post_id"] = 
+	# 	data["author_name"] =
 
 	
-get_json_all()
+# get_json_all()
+# a, b = get_author_name('116151972998_10153811117102999')
+# print a, b
+# a = get_json_all()
+# print a
+
+def get_json_test():
+	dict_test = []
+	c = ''
+	for a in range(0,10):
+		b = get_json_all(a)
+		b = str(b)
+		c = c + ',' + b
+		# dict_test.append(b)
+		
+	dict_test.append(c)
+	json_data1 = json.loads(json.dumps(dict_test, ensure_ascii=False))
+	return json_data1
+
+def get_json_test():
+	dict_test = []
+	c = ''
+	for a in range(10,25):
+		b = get_json_all(a)
+		b = str(b)
+		c = c + ',' + b
+		# dict_test.append(b)
+		
+	dict_test.append(c)
+	json_data1 = json.loads(json.dumps(dict_test, ensure_ascii=False))
+	return json_data1
+
+
+# get_json_test()
+a = get_data_from_page_id(28)
+print a
