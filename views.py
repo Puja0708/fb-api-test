d83d2a877da478891c0a8ba0761d4323e2be2e5e
Puja <singh.puja92@hotmail.com>
2016-01-15 02:36:56 +0530

    Update views.py

--- views.py
+++ views.py
@@ -6,13 +6,7 @@ import requests
 import sqlite3
 
 
-
-
-
-
-
-
-ACCESS_TOKEN = 'CAACEdEose0cBAOaVxWbj6ZAEvZAuuB4jtQB1iWBHUJlqLnSeSZChfa7jSLyhOJ0b6aAZABLLTWIcOBssxMaqw5WxktZBU19I3jKUXhdMBzLPsiYhZAMEgCUQkTdkktELAvGXB72vidjuuThW3ZCaDPK5D9n2v5j0ZB2MUBngnF88n1BUHblq5M3IVq7lB07G6rDRHj99IHHY3CR1gWERHjcv'
+ACCESS_TOKEN = 'CAACEdEose0cBALqa8gpn4p3kSt7C6UM9f7XrQlIoHpSJHvtLAMUAVd1xeDH8BJ9vEWQeJ8rx1W9wxQpmnxoebEptaJKVHitZBHhYdbPeImoZBjtQq6jIKIjzEpXSU4gWwcD9GrwiU6M9eFjUd2wVQhPLrif67xJ9AwJIuqRZAelq3gUcoT4gbpCbuTPWuO1HXoGggD6ZAwZDZD'
 DEBUG = True
 app = Flask(__name__)
 app.debug = DEBUG
@@ -26,24 +20,25 @@ base_url = 'https://graph.facebook.com/v2.2/'
 likes = '116151972998_10153804678607999/likes?access_token=' #replace the post_id
 post = '116151972998_10153804678607999?access_token='
 
-post_urls=['https://graph.facebook.com/v2.2/116151972998/posts?limit=25&__paging_token=enc_AdA3CLIwZBCJLuTek2P1DMJM9SXt5xp3Ye5TYQZAqDSrBgoAOMOSmqA0f69XtdY4wLFgfuCgiJeVO0ETihhDQh2EihZBkLjVeQg051D2ykYpu8BNwZDZD&access_token=CAACEdEose0cBABL3qKfnZCa4CauG8xga8G0SJboD1vr2pEXQudbZBNJgqwsP1WRMQR9HITYVc3ZBdSNygZAarCgYbY87ygXS2xH2Ju2OZBpJjVZAfzgPwGh9XnecAs0DUHeKKiWAmXXMqll95raZAUjsJT61rJlMRRZBlNy34G7itBnst6r4nByt1fSkey4e4MLMBARATJEZCg8ZBkMxp3gOVM&until=1452711835', 'https://graph.facebook.com/v2.2/116151972998/posts?limit=25&__paging_token=enc_AdDKbrrnMtVUWO9Hozbh5RVFHuYpEC8pWcERXPs9wpsWuyWMjmgsgo8fstV4PfsbguBaMlvt3CEmnPUtvZA5GEMI5iGIeci9WzvBTyYDZAE2tC2gZDZD&access_token=CAACEdEose0cBABL3qKfnZCa4CauG8xga8G0SJboD1vr2pEXQudbZBNJgqwsP1WRMQR9HITYVc3ZBdSNygZAarCgYbY87ygXS2xH2Ju2OZBpJjVZAfzgPwGh9XnecAs0DUHeKKiWAmXXMqll95raZAUjsJT61rJlMRRZBlNy34G7itBnst6r4nByt1fSkey4e4MLMBARATJEZCg8ZBkMxp3gOVM&until=1452691879', 'https://graph.facebook.com/v2.2/116151972998/posts?limit=25&__paging_token=enc_AdBiWwwJbAZBYuv49gAjvJJYwqSdZCwOTwOWA7JoR4OOSvdeZB6eVzEnpoPYLZBdJpXOBCCGhhzaxPNiLHYn55ahXZCc9ep8NxAClHUmLmfkxs0qODAZDZD&access_token=CAACEdEose0cBABL3qKfnZCa4CauG8xga8G0SJboD1vr2pEXQudbZBNJgqwsP1WRMQR9HITYVc3ZBdSNygZAarCgYbY87ygXS2xH2Ju2OZBpJjVZAfzgPwGh9XnecAs0DUHeKKiWAmXXMqll95raZAUjsJT61rJlMRRZBlNy34G7itBnst6r4nByt1fSkey4e4MLMBARATJEZCg8ZBkMxp3gOVM&until=1452671012', 'https://graph.facebook.com/v2.2/116151972998/posts?limit=25&__paging_token=enc_AdAqbHZAtW3Se36AWc9WbL8HzlI7qCkiNB4EJAB9o1XDF7rjFS11fNk0OaGxc9ZByE3JsIqJWMz0Mtvv12B1xINSPfCCqaxkgjggwe5VhyWYsf1wZDZD&access_token=CAACEdEose0cBABL3qKfnZCa4CauG8xga8G0SJboD1vr2pEXQudbZBNJgqwsP1WRMQR9HITYVc3ZBdSNygZAarCgYbY87ygXS2xH2Ju2OZBpJjVZAfzgPwGh9XnecAs0DUHeKKiWAmXXMqll95raZAUjsJT61rJlMRRZBlNy34G7itBnst6r4nByt1fSkey4e4MLMBARATJEZCg8ZBkMxp3gOVM&until=1452622077', 'https://graph.facebook.com/v2.2/116151972998/posts?limit=25&__paging_token=enc_AdBCwjTb6wrs9w07B7PgIUcGvZCmSH3tY5P3qndtYbWJD1bpzJM2KHDhWlnvtWDnbDqKaqmxGrQdfnmf5XR5jxwlZB3qLkCaVZAwEc3s5HNl8eUyAZDZD&access_token=CAACEdEose0cBABL3qKfnZCa4CauG8xga8G0SJboD1vr2pEXQudbZBNJgqwsP1WRMQR9HITYVc3ZBdSNygZAarCgYbY87ygXS2xH2Ju2OZBpJjVZAfzgPwGh9XnecAs0DUHeKKiWAmXXMqll95raZAUjsJT61rJlMRRZBlNy34G7itBnst6r4nByt1fSkey4e4MLMBARATJEZCg8ZBkMxp3gOVM&until=1452601212', 'https://graph.facebook.com/v2.2/116151972998/posts?limit=25&__paging_token=enc_AdDlYHg61Ee3jMOpjN3FAVoDXvMOvaOCCRytGkKsgWxWw3M1lJ01fMNx6PLogstmpYAd2xAakQB26dVPFyCZAqC7TQQHK8f6tJFCXRQs4Wq9iiQZDZD&access_token=CAACEdEose0cBABL3qKfnZCa4CauG8xga8G0SJboD1vr2pEXQudbZBNJgqwsP1WRMQR9HITYVc3ZBdSNygZAarCgYbY87ygXS2xH2Ju2OZBpJjVZAfzgPwGh9XnecAs0DUHeKKiWAmXXMqll95raZAUjsJT61rJlMRRZBlNy34G7itBnst6r4nByt1fSkey4e4MLMBARATJEZCg8ZBkMxp3gOVM&until=1452577187', 'https://graph.facebook.com/v2.2/116151972998/posts?limit=25&__paging_token=enc_AdCN0m9MF1SIr5lPRKsZCWsWZC97O2dmnbNT6VzTEVpaiE8rhk6b0obELZCQYVienAafS9B4HJuxLctUgmdT7ChHzWHZBZCoIu4PXsXaQkWyTG6sOygZDZD&access_token=CAACEdEose0cBABL3qKfnZCa4CauG8xga8G0SJboD1vr2pEXQudbZBNJgqwsP1WRMQR9HITYVc3ZBdSNygZAarCgYbY87ygXS2xH2Ju2OZBpJjVZAfzgPwGh9XnecAs0DUHeKKiWAmXXMqll95raZAUjsJT61rJlMRRZBlNy34G7itBnst6r4nByt1fSkey4e4MLMBARATJEZCg8ZBkMxp3gOVM&until=1452535160', 'https://graph.facebook.com/v2.2/116151972998/posts?limit=25&__paging_token=enc_AdDEWRfx7eAM2BaP6tPqzZCX1LZB8TOU37yqxo4r2Bm5sCvd4VMmgZC8iSIjZCj3eepABP4cZAIJSRFBnPPcQjTRNBVksekZBCDNNIX1cZAq7L3J94chAZDZD&access_token=CAACEdEose0cBABL3qKfnZCa4CauG8xga8G0SJboD1vr2pEXQudbZBNJgqwsP1WRMQR9HITYVc3ZBdSNygZAarCgYbY87ygXS2xH2Ju2OZBpJjVZAfzgPwGh9XnecAs0DUHeKKiWAmXXMqll95raZAUjsJT61rJlMRRZBlNy34G7itBnst6r4nByt1fSkey4e4MLMBARATJEZCg8ZBkMxp3gOVM&until=1452519420', 'https://graph.facebook.com/v2.2/116151972998/posts?limit=25&__paging_token=enc_AdBnTS01yxKX6pXnDYXglPIZCHbwNLh6cFKXOOqiIEZC0dBZAxQ2Rk3Gg84XcLJf32DZCw0dFkQ1suSQWenoqoCGK1ZCTTkjFjG5N6VW6FwqCjjk2uAZDZD&access_token=CAACEdEose0cBABL3qKfnZCa4CauG8xga8G0SJboD1vr2pEXQudbZBNJgqwsP1WRMQR9HITYVc3ZBdSNygZAarCgYbY87ygXS2xH2Ju2OZBpJjVZAfzgPwGh9XnecAs0DUHeKKiWAmXXMqll95raZAUjsJT61rJlMRRZBlNy34G7itBnst6r4nByt1fSkey4e4MLMBARATJEZCg8ZBkMxp3gOVM&until=1452499503', 'https://graph.facebook.com/v2.2/116151972998/posts?limit=25&__paging_token=enc_AdDhefyVXQ3vDXwspezwIqjXWrTjLOfS1eUoZCrT6NOcDRIErluVfBUAgUow4GjlJpkQAaEPEexoXINZCjF0fV8yVgaqZChdAngZBlZCTDQPUkzTZBTQZDZD&access_token=CAACEdEose0cBABL3qKfnZCa4CauG8xga8G0SJboD1vr2pEXQudbZBNJgqwsP1WRMQR9HITYVc3ZBdSNygZAarCgYbY87ygXS2xH2Ju2OZBpJjVZAfzgPwGh9XnecAs0DUHeKKiWAmXXMqll95raZAUjsJT61rJlMRRZBlNy34G7itBnst6r4nByt1fSkey4e4MLMBARATJEZCg8ZBkMxp3gOVM&until=1452449160']
-
+post_urls=['', '', '', '', '', '', '', '', '']
 #we are using the facebook graph API version 2.2, instead of the latest one(2.5)
-def get_data_from_page_id(url,i):
+def get_data_from_page_id(i):
 	
-	url = url
+	url = 'https://graph.facebook.com/v2.2/116151972998/posts?limit=25&__paging_token=enc_AdDvolaldgdtLd94ewPuQSnZAfzyMAFiZAqURTjMONBnZBZAAejyDtHPCuox1LINOnSeKxiK6oqMWJlVD7U1GiKqP6WKYgByJYMzATP4WkH2rGk7DgZDZD&access_token=CAACEdEose0cBAA5JAUmAzW7gXUCPfeBl9BUgGga20ZBBU6jZC7xdi59Wf5FBeQvkZAZBArihik8gWJfCZCaBMP8PLrzXexT14sB5hmx1ZCc26uNqlJ7U9WBTNfNiyGzWrhJ4dv9udnEj4i8MxShQdOpsIH6ItRROoO7IjHal77ty40GKa03b6MXQpW5gB9twAZD&until=1452756420'
 	# response = requests.get(url)
 	json_data = connect_url(url)
 	# connect_url(url)
 	data1 = json_data["data"]
+	# print json_data
+	# print data1
 	post_id_to_be_returned = data1[i]["id"]
 		# return post_id_to_be_returned
 		# post_id_to_be_returned = data1[i]["id"]
 	# title = data1[i]["name"]
-	if i>25:
-		if 'next' in json_data['paging']:
-			url = json_data["paging"]["next"]
-			post_id_to_be_returned = url
+	# if i>25:
+	# 	if 'next' in json_data['paging']:
+	# 		url = json_data["paging"]["next"]
+	# 		post_id_to_be_returned = url
 	# print i, title, post_id_to_be_returned
 	return post_id_to_be_returned
 	
@@ -63,7 +58,7 @@ def get_data_from_page_id(url,i):
 
 def get_data_from_post_id():
 	# return "hello"
-	url = 'https://graph.facebook.com/v2.2/116151972998/posts?access_token=CAACEdEose0cBAOaVxWbj6ZAEvZAuuB4jtQB1iWBHUJlqLnSeSZChfa7jSLyhOJ0b6aAZABLLTWIcOBssxMaqw5WxktZBU19I3jKUXhdMBzLPsiYhZAMEgCUQkTdkktELAvGXB72vidjuuThW3ZCaDPK5D9n2v5j0ZB2MUBngnF88n1BUHblq5M3IVq7lB07G6rDRHj99IHHY3CR1gWERHjcv'
+	url = 'https://graph.facebook.com/v2.2/116151972998/posts?access_token=CAACEdEose0cBALqa8gpn4p3kSt7C6UM9f7XrQlIoHpSJHvtLAMUAVd1xeDH8BJ9vEWQeJ8rx1W9wxQpmnxoebEptaJKVHitZBHhYdbPeImoZBjtQq6jIKIjzEpXSU4gWwcD9GrwiU6M9eFjUd2wVQhPLrif67xJ9AwJIuqRZAelq3gUcoT4gbpCbuTPWuO1HXoGggD6ZAwZDZD'
 	json_data = connect_url(url)
 	number_of_posts = len(json_data["data"])
 
@@ -72,12 +67,32 @@ def get_data_from_post_id():
 def get_number_of_likes_on_post(id):
 	# url = base_url + likes + ACCESS_TOKEN
 	url = base_url + id + '/likes?access_token=' + ACCESS_TOKEN
-	
-	json_data = connect_url(url)
-	length = len(json_data)
-	data1 = json_data["data"]
-	number_of_likes = len(data1)
-	return number_of_likes
+	response = requests.get(url)
+	json_data = json.loads(response.text)
+	likes = json_data['data']
+	next_url1 = json_data['paging']['next']
+	next_url = []
+	count = 1
+	like_count = 0
+	# print next_url1
+	if json_data['paging'] and 'next' in json_data['paging'].keys() and count<51:
+		i = 1
+	while(i):
+		response = requests.get(next_url1)
+		json_data = json.loads(response.text)
+		likes = json_data['data']
+		like_count = like_count + len(json_data['data'])
+		if json_data['paging'] and 'next' in json_data['paging'].keys() and count<51:
+			next_url1 = json_data['paging']['next']
+			i = 1
+		else:
+			i = 0
+			# next_url1 = 'exit'
+		# next_url.append(next_url1)
+		# print i
+		count = count+1
+	print count
+	return like_count
 	# return number_of_likes
 
 # def get_comment_count_on_post():
@@ -89,7 +104,8 @@ def get_number_of_likes_on_post(id):
 
 def get_author_name(id):
 	# url = base_url + post + ACCESS_TOKEN
-	url = 'https://graph.facebook.com/v2.2/' + id +'?access_token=CAACEdEose0cBAOaVxWbj6ZAEvZAuuB4jtQB1iWBHUJlqLnSeSZChfa7jSLyhOJ0b6aAZABLLTWIcOBssxMaqw5WxktZBU19I3jKUXhdMBzLPsiYhZAMEgCUQkTdkktELAvGXB72vidjuuThW3ZCaDPK5D9n2v5j0ZB2MUBngnF88n1BUHblq5M3IVq7lB07G6rDRHj99IHHY3CR1gWERHjcv'
+	url = 'https://graph.facebook.com/v2.2/' + id +'?access_token=CAACEdEose0cBAA5JAUmAzW7gXUCPfeBl9BUgGga20ZBBU6jZC7xdi59Wf5FBeQvkZAZBArihik8gWJfCZCaBMP8PLrzXexT14sB5hmx1ZCc26uNqlJ7U9WBTNfNiyGzWrhJ4dv9udnEj4i8MxShQdOpsIH6ItRROoO7IjHal77ty40GKa03b6MXQpW5gB9twAZD'
+	print url
 	json_data = connect_url(url)
 	author_name = json_data["admin_creator"]["name"]
 	# post_id = json_data["id"]
@@ -100,7 +116,7 @@ def get_author_name(id):
 
 def get_data_for_json(id):
 	# url = base_url + post + ACCESS_TOKEN
-	url = 'https://graph.facebook.com/v2.2/' + id +'?access_token=CAACEdEose0cBAOaVxWbj6ZAEvZAuuB4jtQB1iWBHUJlqLnSeSZChfa7jSLyhOJ0b6aAZABLLTWIcOBssxMaqw5WxktZBU19I3jKUXhdMBzLPsiYhZAMEgCUQkTdkktELAvGXB72vidjuuThW3ZCaDPK5D9n2v5j0ZB2MUBngnF88n1BUHblq5M3IVq7lB07G6rDRHj99IHHY3CR1gWERHjcv'
+	url = 'https://graph.facebook.com/v2.2/' + id +'?access_token=CAACEdEose0cBALqa8gpn4p3kSt7C6UM9f7XrQlIoHpSJHvtLAMUAVd1xeDH8BJ9vEWQeJ8rx1W9wxQpmnxoebEptaJKVHitZBHhYdbPeImoZBjtQq6jIKIjzEpXSU4gWwcD9GrwiU6M9eFjUd2wVQhPLrif67xJ9AwJIuqRZAelq3gUcoT4gbpCbuTPWuO1HXoGggD6ZAwZDZD'
 	json_data = connect_url(url)
 	author_name = json_data["admin_creator"]["name"]
 	# post_id = id
@@ -238,7 +254,7 @@ def connect_url(url):
 # print a
 
 def database_test():
-	sqlite_file = '/home/puja/Documents/fb-api-test/fb-api.db'
+	sqlite_file = '/home/puja/Documents/fb-test/fb-api-test/fb-api.db'
 	table_name1 = 'fb_api_test'
 
 	conn = sqlite3.connect(sqlite_file)
@@ -251,15 +267,96 @@ def database_test():
 	# author = 'dddgfdee'
 	# likes = 14
 	i = 0
-	count1 = len(post_urls)
+	# count1 = len(post_urls)
 	# for i in range(1,count1):
-	for post_url in post_urls:
-		for i in range(0,25):
-			post_id = get_data_from_page_id(post_url, i)
-			post_title, author, link, time = get_author_name(post_id)
-			likes = get_number_of_likes_on_post(post_id)
+	# for post_url in post_urls:
+	for i in range(0,25):
+		post_id = get_data_from_page_id(i)
+		post_title, author, link, time = get_author_name(post_id)
+			# likes = get_number_of_likes_on_post(post_id)
 
-			c.execute("insert into fb_api_test(post_id, post_title, author, likes, post_link, post_updated_time) values (?,?,?,?,?,?)",(post_id, post_title, author, likes, link, time))
+		c.execute("insert into fb_api_test(post_id, post_title, author, post_link, post_updated_time) values (?,?,?,?,?)",(post_id, post_title, author, link, time))
+		print "one row done"
+		# c.execute("insert into fb_api_test(post_title) values (?)",(post_title,))
+		# c.execute("insert into fb_api_test(author) values (?)",(author,))
+		# c.execute("insert into fb_api_test(likes) values (?)",(likes,))
+
+	conn.commit()
+	conn.close()
+	print "done"
+
+def database__likes_test():
+	sqlite_file = '/home/puja/Documents/fb-test/fb-api-test/fb-api.db'
+	table_name1 = 'fb_api_test'
+
+	conn = sqlite3.connect(sqlite_file)
+	c = conn.cursor()
+	c1 = conn.cursor()
+	c2 = conn.cursor()
+	author_names = []
+	post_ids = []
+
+	# c.execute('CREATE TABLE IF NOT EXISTS fb_api_test (post_id integer, post_title text, author text, likes integer, post_link text, post_updated_time text)')
+	query1 = 'SELECT DISTINCT author FROM fb_api_test;'
+	query2 = 'SELECT DISTINCT post_id FROM fb_api_test;'
+	# 
+	# rows = c.execute(query1)
+	rows1 = c1.execute(query2)
+	# print rows
+	for row in c.fetchall():
+		# print(row)
+		row1 = json.dumps(row)
+		# str(row)
+		# row = row.split("'")
+		# row=row[1]
+		author_names.append(row1)
+
+	for name in author_names:
+		# print name
+		name1 = name.split('"')
+		# print name1[1]
+		# print ''
+		query2 = 'update table'
+	for pid in c1.fetchall():
+		post_ids.append(pid)
+	# for pid1 in post_ids:
+	# 	# str(pid1)
+	# 	pid2 = pid1.replace(u',')
+	# 	# pid2 = pid2[1]
+	# 	print pid2
+	# print post_ids
+	# post_ids = json.dumps(post_ids)
+	# print len(post_ids)
+	# print post_ids
+	
+	for post_id in post_ids:
+		# post_id = post_ids[i]
+		print str(post_id)
+		post_id1 = post_id.split("u")
+		post_id1 = post_id1[1]
+		print post_id1
+
+		likes = 100
+		query3 = 'UPDATE TABLE fb_api_test SET likes=(?) WHERE post_id=(?)',(likes, post_id)
+		c2.execute(query3)
+
+
+	# print author_names, author_names[4]
+	# post_id = 1234588
+	# post_title = 'aaaaghaaaaaaabb'
+	# author = 'dddgfdee'
+	# likes = 14
+	# i = 0
+	# count1 = len(post_urls)
+	# for i in range(1,count1):
+	# for post_url in post_urls:
+	# for i in range(0,25):
+	# 	post_id = get_data_from_page_id(i)
+	# 	post_title, author, link, time = get_author_name(post_id)
+	# 		# likes = get_number_of_likes_on_post(post_id)
+
+	# 	c.execute("insert into fb_api_test(post_id, post_title, author, post_link, post_updated_time) values (?,?,?,?,?)",(post_id, post_title, author, link, time))
+	# 	print "one row done"
 		# c.execute("insert into fb_api_test(post_title) values (?)",(post_title,))
 		# c.execute("insert into fb_api_test(author) values (?)",(author,))
 		# c.execute("insert into fb_api_test(likes) values (?)",(likes,))
@@ -269,7 +366,7 @@ def database_test():
 	print "done"
 
 def get_all_posts():
-	url = 'https://graph.facebook.com/v2.2/116151972998/posts?access_token=CAACEdEose0cBAOaVxWbj6ZAEvZAuuB4jtQB1iWBHUJlqLnSeSZChfa7jSLyhOJ0b6aAZABLLTWIcOBssxMaqw5WxktZBU19I3jKUXhdMBzLPsiYhZAMEgCUQkTdkktELAvGXB72vidjuuThW3ZCaDPK5D9n2v5j0ZB2MUBngnF88n1BUHblq5M3IVq7lB07G6rDRHj99IHHY3CR1gWERHjcv'
+	url = 'https://graph.facebook.com/v2.2/116151972998/posts?access_token=CAACEdEose0cBALqa8gpn4p3kSt7C6UM9f7XrQlIoHpSJHvtLAMUAVd1xeDH8BJ9vEWQeJ8rx1W9wxQpmnxoebEptaJKVHitZBHhYdbPeImoZBjtQq6jIKIjzEpXSU4gWwcD9GrwiU6M9eFjUd2wVQhPLrif67xJ9AwJIuqRZAelq3gUcoT4gbpCbuTPWuO1HXoGggD6ZAwZDZD'
 	# response = requests.get(url)
 	all_posts = []
 	count = 0
@@ -283,7 +380,7 @@ def get_all_posts():
 	print count
 
 def get_likes_count():
-	url = 'https://graph.facebook.com/v2.2/116151972998_10153825139507999/likes?access_token=CAACEdEose0cBAOaVxWbj6ZAEvZAuuB4jtQB1iWBHUJlqLnSeSZChfa7jSLyhOJ0b6aAZABLLTWIcOBssxMaqw5WxktZBU19I3jKUXhdMBzLPsiYhZAMEgCUQkTdkktELAvGXB72vidjuuThW3ZCaDPK5D9n2v5j0ZB2MUBngnF88n1BUHblq5M3IVq7lB07G6rDRHj99IHHY3CR1gWERHjcv'
+	url = 'https://graph.facebook.com/v2.2/116151972998_10153825139507999/likes?access_token=CAACEdEose0cBALqa8gpn4p3kSt7C6UM9f7XrQlIoHpSJHvtLAMUAVd1xeDH8BJ9vEWQeJ8rx1W9wxQpmnxoebEptaJKVHitZBHhYdbPeImoZBjtQq6jIKIjzEpXSU4gWwcD9GrwiU6M9eFjUd2wVQhPLrif67xJ9AwJIuqRZAelq3gUcoT4gbpCbuTPWuO1HXoGggD6ZAwZDZD'
 	
 	# likes = []
 	likes_count = 1
@@ -305,7 +402,7 @@ def get_likes_count():
 
 def fb_paging_test():
 	# url = 'https://graph.facebook.com/v2.2/116151972998_10153825139507999/likes?access_token=CAACEdEose0cBAOGlgBlnDPbqA0dhsfwzfdPSZAydk47EqOu4axjOV7ddiR4aUhVP8XU7ZC4SIqyqZCg6zFIyZBPpaJvFL9Ji91mG9LFzmyW9Bc8smmxJ9RYOxcsAaPViM426WraqY5I7L8148xGv7inItQmDxvtPTEiZA7TAULrwccxWOoOfTZCLazP2kd7oqK8CXqKUYhlgZDZD'
-	url = 'https://graph.facebook.com/v2.2/116151972998/posts?access_token=CAACEdEose0cBAOaVxWbj6ZAEvZAuuB4jtQB1iWBHUJlqLnSeSZChfa7jSLyhOJ0b6aAZABLLTWIcOBssxMaqw5WxktZBU19I3jKUXhdMBzLPsiYhZAMEgCUQkTdkktELAvGXB72vidjuuThW3ZCaDPK5D9n2v5j0ZB2MUBngnF88n1BUHblq5M3IVq7lB07G6rDRHj99IHHY3CR1gWERHjcv'
+	url = 'https://graph.facebook.com/v2.2/116151972998/posts?access_token=CAACEdEose0cBAA5JAUmAzW7gXUCPfeBl9BUgGga20ZBBU6jZC7xdi59Wf5FBeQvkZAZBArihik8gWJfCZCaBMP8PLrzXexT14sB5hmx1ZCc26uNqlJ7U9WBTNfNiyGzWrhJ4dv9udnEj4i8MxShQdOpsIH6ItRROoO7IjHal77ty40GKa03b6MXQpW5gB9twAZD'
 	response = requests.get(url)
 	json_data = json.loads(response.text)
 	likes = json_data['data']
@@ -331,9 +428,10 @@ def fb_paging_test():
 	print next_url, count
 
 
-	
-
-database_test()
+# get_data_from_page_id(6)	
+# fb_paging_test()
+# database_test()
+database__likes_test()
 
 # def get_total_number_of_posts_on_page():
 # 	url = 'https://graph.facebook.com/v2.2/116151972998/posts?access_token=CAACEdEose0cBAKTE1SgAIrAKkwi6WLMtg4QN28ZCkNDebFCZCK0c70MwEjnKIltZCJtz5PSJcZAzZC6zZAUx3p1sEkXzZCTkPTs92gb7bZCbIIgOThQwrWiTFkt1VcoTwCjibZBM6X89iNdjGBRI2aKw437kKkosVdBV44bVxJUUScuFdPCBcT6wWtqPZAhTuag1pmbZArS95I2KQwZATK1C77k4'
