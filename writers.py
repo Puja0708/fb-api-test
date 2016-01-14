12f59744cb9bdf7315ef47545bd56a75ec258554
puja_sk <puja@sportskeeda.com>
2016-01-14 20:02:31 +0530

    database connnection done

--- writers.py
+++ writers.py
@@ -1,15 +1,16 @@
-from mysql.connector import MySQLConnection, Error
-from python_mysql_dbconfig import read_db_config
+import MySQLdb
 
 DEBUG = True
-database_location = '/var/lib/mysql/sportskeeda'
+database_location = '/var/lib/mysql/'
 table_name = 'keeda_author_fact'
-
-query = 'select author_id, author_url, author_title, author_post_count, total_reads, author_rss from keeda_author_fact ORDER BY author_post_count'
-dbconfig = read_db_config()
-conn = MySQLConnection(**dbconfig)
-cursor = conn.cursor()
+db = MySQLdb.connect('localhost', 'sportskeeda', '', 'sportskeeda')
+query = 'select author_id, author_url, author_title, author_post_count, total_reads, author_rss from keeda_author_fact ORDER BY author_post_count limit 5'
+# ?dbconfig = read_db_config()
+# conn = MySQLConnection(**dbconfig)
+cursor = db.cursor()
 cursor.execute(query)
 rows = cursor.fetchall()
+
+
 
 print rows
